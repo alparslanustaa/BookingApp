a01d994d4eea2149dfd592c28ebea2a075b5a148
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-06-15 08:18:56 -0400

    navbar bug fix

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,12 @@
-import React from "react";
+import "./home.css";
+import Navbar from "../../components/navbar/Navbar";
 
 const home = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default home;
