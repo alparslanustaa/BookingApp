ae6066c1afeb970aaca52eb781ed4e40e4bc2d6f
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-06-18 23:56:45 -0400

    header btn style and create

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,27 +10,34 @@ import "./header.css";
 const Header = () => {
   return (
     <div className="header">
-      <div className="headerList">
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faBed} />
-          <span>Stays</span>
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport taxis</span>
+          </div>
         </div>
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faPlane} />
-          <span>Flights</span>
-        </div>
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faCar} />
-          <span>Car rentals</span>
-        </div>
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faBed} />
-          <span>Attractions</span>
-        </div>
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faTaxi} />
-          <span>Airport taxis</span>
-        </div>
+        <h1 className="headerTitle">A life time travel? </h1>
+        <p className="headerDesc">
+          Get some rewards for your travel- unlock instant coupons.
+        </p>
+        <button className="headerBtn">Register/Login</button>
       </div>
     </div>
   );
